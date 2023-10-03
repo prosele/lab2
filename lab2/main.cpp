bce298afb181b041b25dd15a557cc7382040508c
Kseniia Prokopovich <prokopovichksenia@gmail.com>
2023-10-03 13:55:57 +0300

    com2

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
-#include <cmath>
+
+#include <iomanip>
 using namespace std;
 long double fact(int a){
-    if (a==0 or a==1) {
-        return 1;
-    }
-    
-    return a*fact(a-2);
+    return (a==0 or a==1) ? 1 : a*fact(a-2);
         
+}
+long double pow(long double d, long long n) {
+    long double um=1;
+    while (n > 0) {
+        um*=d;
+        n--;
+    }
+    return um;
 }
 long double t(long double x){
     long double sum1=0, sum2=0;
@@ -24,6 +29,8 @@ int main() {
     long double y;
     cin >> y;
     long double result = (7*t(0.25)+2*t(1+y))/(6-t(y*y-1));
+    cout << setprecision(16) << fixed;
     cout << result << endl;
+    
     return 0;
 }
